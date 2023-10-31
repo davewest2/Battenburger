9e756887a5f5ee12895690a46701dd4ebee8c1a4
Dave West <davewest2@hotmail.co.uk>
2023-10-31 09:08:25 +0000

    SelectPhoto viewmodel implemented

--- app/src/main/java/com/example/battenburger/MainActivity.kt
+++ app/src/main/java/com/example/battenburger/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.battenburger
 
 import android.graphics.Bitmap
-import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
@@ -15,13 +14,11 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
 import coil.compose.AsyncImage
+import com.example.battenburger.domain.SelectPhotoViewModel
 import com.example.battenburger.domain.convertUriToBitmap
 import com.example.battenburger.domain.pixelManipulator
 import com.example.battenburger.domain.saveImageToInternalStorage
@@ -32,6 +29,8 @@ const val TAG = "TAG"
 lateinit var imageBitMap: Bitmap
 
 class MainActivity : ComponentActivity() {
+
+    private val selectPhotoViewModel = SelectPhotoViewModel()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -39,19 +38,19 @@ class MainActivity : ComponentActivity() {
             //Navigation()
             BattenburgerTheme {
                 Log.d(TAG, "starting app")
-                val stockBitmap = Bitmap.createBitmap(20, 20, Bitmap.Config.ARGB_8888)
+                //val stockBitmap = Bitmap.createBitmap(20, 20, Bitmap.Config.ARGB_8888)
                 val context = LocalContext.current
                 val files: Array<String> = context.fileList()
-                var selectedImageUri by remember {
-                    mutableStateOf<Uri?>(null)
-                }
-                var quadBitMapToDisplay by remember {
-                    mutableStateOf<Bitmap>(stockBitmap)
-                }
+                //var selectedImageUri by remember {
+                //    mutableStateOf<Uri?>(null)
+                //}
+//                var quadBitMapToDisplay by remember {
+//                    mutableStateOf<Bitmap>(stockBitmap)
+//                }
 
                 val photoPickerLauncher = rememberLauncherForActivityResult(
                     contract = ActivityResultContracts.GetContent(),
-                    onResult = { contentUri -> selectedImageUri = contentUri }
+                    onResult = { contentUri -> selectPhotoViewModel.selectedImageUri = contentUri }
                 )
 
                 LazyColumn(
@@ -75,39 +74,52 @@ class MainActivity : ComponentActivity() {
                     item {
                         Row(
                             modifier = Modifier.fillMaxWidth(),
-                            horizontalArrangement = Arrangement.SpaceAround
+                            horizontalArrangement = Arrangement.SpaceEvenly
                         ) {
-                            Button(onClick = {
-                                saveImageToInternalStorage(context, selectedImageUri!!)
+                            Button(
+                                modifier = Modifier.weight(0.33f),
+                                onClick = {
+                                saveImageToInternalStorage(context, selectPhotoViewModel.selectedImageUri!!)
                                 Log.d(TAG, "image saved to internal storage, filename ${files[8]} which should be image.jpg")
                             }
+
                             ) {
-                                Text(text = "Save image")
+                                Text(
+                                    textAlign = TextAlign.Center,
+                                    text = "Save image")
                             }
-                            Button(onClick = {
+                            Button(
+                                modifier = Modifier.weight(0.33f),
+                                onClick = {
                                 imageBitMap = convertUriToBitmap(context)
                                 Log.d(TAG, "uri converted to bitmap")
                             }
                             ) {
-                                Text(text = "Convert image to bitmap")
+                                Text(
+                                    textAlign = TextAlign.Center,
+                                    text = "Convert to bitmap")
                             }
 
-                            Button(onClick = {
-                                quadBitMapToDisplay = pixelManipulator(imageBitMap)
+                            Button(
+                                modifier = Modifier.weight(0.33f),
+                                onClick = {
+                                selectPhotoViewModel.quadBitMapToDisplay = pixelManipulator(imageBitMap)
                                 Log.d(TAG, "Join images button pressed")
                             }
                             ) {
-                                Text(text = "Battenburg image")
+                                Text(
+                                    textAlign = TextAlign.Center,
+                                    text = "Battenburg image")
                             }
                         }
 
                         AsyncImage(
-                            model = quadBitMapToDisplay,
+                            model = selectPhotoViewModel.quadBitMapToDisplay,
                             contentDescription = "my photo battenburged",
                             modifier = Modifier.fillMaxSize(),
                         )
                         Log.d(TAG, "Async quadbitmap image displayed")
-                        Log.d(TAG, "Size of quadbitmap image is ${quadBitMapToDisplay.width} wide and ${quadBitMapToDisplay.height} high")
+                        Log.d(TAG, "Size of quadbitmap image is ${selectPhotoViewModel.quadBitMapToDisplay.width} wide and ${selectPhotoViewModel.quadBitMapToDisplay.height} high")
 
                     }
                 }
--- app/src/main/java/com/example/battenburger/domain/SelectPhotoViewModel.kt
+++ app/src/main/java/com/example/battenburger/domain/SelectPhotoViewModel.kt
@@ -1,6 +1,16 @@
 package com.example.battenburger.domain
 
-class SelectPhotoViewModel {
+import android.graphics.Bitmap
+import android.net.Uri
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
 
+class SelectPhotoViewModel: ViewModel() {
+
+    val stockBitmap = Bitmap.createBitmap(20, 20, Bitmap.Config.ARGB_8888)
+    var selectedImageUri by mutableStateOf<Uri?>(Uri.parse("android.resource://com.example.battenburger/drawable/dave_summer_cross_r2_14_june_2023.bmp"))
+    var quadBitMapToDisplay by mutableStateOf<Bitmap>(stockBitmap)
 
 }
