1ff3bad451c05508c8aaf393b87fdf4a1688ef26
Dave West <davewest2@hotmail.co.uk>
2023-11-15 11:58:38 +0000

    Save function implemented. Permissions not required for saving to media store in API >29 Android 10

--- app/src/main/java/com/example/battenburger/presentation/DisplayBattenburgScreen.kt
+++ app/src/main/java/com/example/battenburger/presentation/DisplayBattenburgScreen.kt
@@ -1,5 +1,7 @@
 package com.example.battenburger.presentation
 
+import android.content.Context
+import android.graphics.Bitmap
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -17,12 +19,49 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
 import com.example.battenburger.domain.DisplayBattenburgViewModel
+import com.example.battenburger.domain.saveImageToMediaStore
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberPermissionState
+import com.google.accompanist.permissions.shouldShowRationale
 
+// TODO: Optional permissions request code commented out.
+//  API level < 29 (Android 10): WRITE_EXTERNAL_STORAGE required
+
+@OptIn(ExperimentalPermissionsApi::class)
 @Composable
 fun DisplayBattenburgScreen(navController: NavController){
     val viewModel = DisplayBattenburgViewModel()
     val context = LocalContext.current
+//    val writeExeternalPermissionState = rememberPermissionState(
+//        android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
 
+    DisplayBattenburg(context,"replace this string", viewModel.battenburg)
+
+//    if (writeExeternalPermissionState.status.isGranted) {
+//        DisplayBattenburg(context,"replace this string", viewModel.battenburg)
+//    } else {
+//        Column {
+//            val textToShow = if (writeExeternalPermissionState.status.shouldShowRationale) {
+//                "Access is required in order to save your Battenburg! Please grant the permission."
+//            } else {
+//                // If it's the first time the user lands on this feature, or the user
+//                // doesn't want to be asked again for this permission, explain that the
+//                // permission is required
+//                "Access permission required for this feature to be available. " +
+//                        "Please grant the permission"
+//            }
+//            Text(textToShow)
+//            Button(onClick = { writeExeternalPermissionState.launchPermissionRequest() }) {
+//                Text("Request permission")
+//            }
+//        }
+//    }
+}
+
+@Composable
+fun DisplayBattenburg(context: Context, displayName: String, bitmap: Bitmap){
+    val viewModel = DisplayBattenburgViewModel()
     Column(
         Modifier
             .fillMaxWidth(),
@@ -42,7 +81,7 @@ fun DisplayBattenburgScreen(navController: NavController){
         Spacer(modifier = Modifier.size(100.dp))
 
         Button(onClick = {
-
+            saveImageToMediaStore(context, displayName, bitmap)
         })
         {
             Text(
@@ -51,7 +90,6 @@ fun DisplayBattenburgScreen(navController: NavController){
         }
     }
 }
-
 
 
 
